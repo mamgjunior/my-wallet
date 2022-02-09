a00df712c3c7797ec3adc1548ac63423f92378b3
LAYOUT\maia <marcosantonio@layoutsistemas.com.br>
2022-02-09 16:56:58 -0300

    Implementando o header do projeto

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import { 
     Container,
-    ToggleLabel
+    ToggleLabel,
+    ToggleSelector
 } from "./styles";
 
 const Toggle: React.FC = () => {
     return (
         <Container>
             <ToggleLabel>Light</ToggleLabel>
+            <ToggleSelector 
+                checked
+                uncheckedIcon={false}
+                checkedIcon={false}
+                onChange={() => console.log('Mudou!!')} 
+            />
             <ToggleLabel>Dark</ToggleLabel>
         </Container>
     );
