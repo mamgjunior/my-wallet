8814c3888a6c6ddba0697819f12e7f76bda09b0b
mamgjunior <mamgjunior@hotmail.com>
2023-01-15 12:54:50 -0300

    implementando a tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import logoImg from '../../assets/logo.svg';
+
+import { 
+    Container, 
+    Logo, 
+    Form,
+    FormTitle 
+} from "./styles";
 
 const SignIn: React.FC = () => {
     return (
-        <div>
-            <h1>SignIn</h1>
-        </div>
+        <Container>
+            <Logo>
+                <img src={logoImg} alt="Minha Carteira" />
+                <h2>Minha Carteira</h2>
+            </Logo>
+            <Form>
+                <FormTitle>Entrar</FormTitle>
+                <input type="text"></input>
+                <input type="text"></input>
+                <button>Acessar</button>
+            </Form>
+        </Container>
     );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import AppRoutes from "./app.routes";
+// import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
-            <AppRoutes />
+            <AuthRoutes />
         </BrowserRouter>
     );
 }
