c015b61b2df48d21e390cf3722adbf6184e89582
mamgjunior <mamgjunior@hotmail.com>
2023-01-22 11:52:08 -0300

    ajuste nos graficos de entrada e saida

--- src/components/BarChartComponent/styles.ts
+++ src/components/BarChartComponent/styles.ts
@@ -13,6 +13,13 @@ export const Container = styled.div`
 
     background-color: ${props => props.theme.colors.tertiary};
     color: ${props => props.theme.colors.white};
+
+    @media(max-width: 1200px) {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: auto;
+    }
 `;
 
 export const SideLeft = styled.aside`
@@ -43,6 +50,11 @@ export const LegendContainer = styled.ul`
     ::-webkit-scrollbar-track {
         background-color: ${props => props.theme.colors.tertiary};
     }
+
+    @media(max-width: 1200px) {
+        display: flex;
+        height: auto;
+    }
 `;
 
 export const Legend = styled.li<ILegend>`
@@ -63,6 +75,15 @@ export const Legend = styled.li<ILegend>`
 
     > span {
         margin-left: 5px;
+    }
+
+    @media(max-width: 1200px) {
+        > div {
+            width: 30px;
+            height: 30px;
+            font-size: 10px;
+            line-height: 30px;
+        }
     }
 `;
 
